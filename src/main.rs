0d9e5f5e3e241e9555a6e976c86ea24658599a79
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-05-02 19:11:39 -0400

    Allow evaluating term directly via CLI or from any file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::fs;
+use anyhow::Context;
+use clap::Parser;
 use lambda::*;
+use std::{fs, path::PathBuf};
 
-fn main() {
-	let file_path = "test.lam";
-	let code =
-		fs::read_to_string(file_path).expect(&format!("should be able to read {}", file_path));
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+	/// Lambda term to evaluate.
+	#[arg(short, long)]
+	term: Option<String>,
+	/// File from which to read a lambda term.
+	#[arg(short, long)]
+	file: Option<PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+	let args = Args::parse();
+	let code = match (args.term, args.file) {
+		(Some(t), None) => t,
+		(None, Some(f)) => {
+			fs::read_to_string(&f).with_context(|| format!("unable to read {}", f.display()))?
+		}
+		(None, None) => anyhow::bail!("no term provided"),
+		(Some(_), Some(_)) => anyhow::bail!("multiple terms provided"),
+	};
 	let mut stream = lex::lex(&code);
 	let e = parse::parse(&mut stream);
-	let output = e
-		.to_debruijn()
-		.eval()
-		.to_named()
-		.to_string();
+	let output = e.to_debruijn().eval().to_named().to_string();
 	println!("{output}");
+	Ok(())
 }
