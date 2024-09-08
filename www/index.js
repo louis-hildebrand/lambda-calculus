2cc433c719c2b8443ac04458d9eeaca1878f084e
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-09-07 23:21:43 -0400

    Add URL param to control result datatype

--- www/index.js
+++ www/index.js
@@ -4,6 +4,22 @@ document.getElementById("input-block").innerHTML = `succ n
 where succ = \\n.\\s.\\z.s(n s z)
 where    n = \\s.\\z.s(s(s(z))) { 3 }`;
 
+const url = new URL(window.location.href);
+const dt = url.searchParams.get("dt");
+const VALID_DATA_TYPES = ["expr", "bool", "church"];
+
+if (VALID_DATA_TYPES.includes(dt)) {
+	document.getElementById("interpret-as").value = dt;
+} else if (!dt) {
+	url.searchParams.set("dt", "expr");
+	window.history.replaceState(null, null, url);
+}
+
+document.getElementById("interpret-as").addEventListener("change", () => {
+	url.searchParams.set("dt", document.getElementById("interpret-as").value);
+	window.history.replaceState(null, null, url);
+});
+
 document.getElementById("eval-btn").addEventListener("click", () => {
 	document.getElementById("output-block").innerText = "...";
 	const e = document.getElementById("input-block").value;
